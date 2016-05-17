9efeab3f4b7440f5ed807228c64b3a4d497835c9
James Collins <jamescol@usc.edu>
2016-05-17 11:51:53 -0700

    Added no internet errors

---
+++ app/src/main/java/fanative/com/stormy/AlertDialogFragment.java
@@ -0,0 +1,21 @@
+package fanative.com.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(R.string.Error_Title)
+                .setMessage(R.string.Error_Message)
+                .setPositiveButton(R.string.PosButtonText, null);
+        return builder.create();
+
+    }
+}
--- app/src/main/java/fanative/com/stormy/MainActivity.java
+++ app/src/main/java/fanative/com/stormy/MainActivity.java
@@ -1,23 +1,89 @@
 package fanative.com.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     //hello
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String apiKey = "b807704e94df3860c103b1fa0982494e";
+        Double latitude = 30.256045;
+        Double longitude = -97.764027;
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
 
+        if (!networkAvailable()){
+            Toast.makeText(this, R.string.noNetwork, Toast.LENGTH_LONG).show();
+        }
+        else {
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+                            Log.v(TAG, getString(R.string.successMessage));
+                        } else {
+                            alertUserError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught", e);
+                    }
+                }
+            });
+            Log.d(TAG, "IT RUNS");
+        }
+
+    }
+
+    private boolean networkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserError() {
+        AlertDialogFragment dialogFragment = new AlertDialogFragment();
+        dialogFragment.show(getFragmentManager(), "errorDialog");
 
     }
 
